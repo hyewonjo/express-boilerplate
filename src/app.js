54ced78ab65a0140f3c559b8d4282f1aebd3bcf6
hyewonjo <hyewonjo92@gmail.com>
2022-09-02 12:26:47 +0900

    loader 구조 적용

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
-require('./config');
-
 const express = require('express');
+const loader = require('./loaders');
 
 module.exports = async function createApp() {
     const app = express();
+    await loader(app);
 
     return app;
 }
