067fd4becef226a156e6643e530e880dfca19745
hyewonjo <hyewonjo92@gmail.com>
2022-09-02 12:27:12 +0900

    server.js - logging 추가

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
-const createApp = require("./app");
+const createApp = require('./app');
+const { logger } = require('./winston');
 
 createApp().then((app) => {
     const appServer = app.listen(app.get('port'), () => {
         process.send('ready');
+        logger.info(`${process.env.NODE_ENV} App listening at http://localhost:${app.get('port')}`);
     });
 
     process.on('SIGINT', () => {
         app.set('isDisableKeepAlive', true);
         appServer.close(function () {
-            console.log('server closed');
+            logger.info('server closed');
             process.exit(0);
         });
     });
