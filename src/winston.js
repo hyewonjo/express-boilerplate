eae95fb5de38f3f7ae6111d4fc4cfaab52a61e00
hyewonjo <hyewonjo92@gmail.com>
2022-09-02 12:03:40 +0900

    winston.js 추가

---
+++ src/winston.js
@@ -0,0 +1,55 @@
+const env = process.env.NODE_ENV || 'development';
+
+const winston = require('winston');
+const winstonDaily = require('winston-daily-rotate-file');
+
+const { combine, timestamp, printf, label } = winston.format;
+const logDir = 'logs';
+const config = require('./config')[env];
+
+const logFormat = combine(
+    timestamp({
+        format: 'YYYY-MM-DDTHH:mm:ssZZ',
+    }),
+    label({
+        label: process.pid,
+    }),
+    printf(info => {
+        return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
+    }),
+);
+
+const logger = winston.createLogger({
+    format: logFormat,
+    transports: [
+        // normal log
+        new winstonDaily({
+            level: config.logLevel,
+            datePattern: 'YYYY-MM-DD',
+            dirname: logDir,
+            filename: '%DATE%.log',
+            maxFiles: 30, // 30 days
+            zippedArchive: true,
+        }),
+        // error log
+        new winstonDaily({
+            level: 'error',
+            datePattern: 'YYYY-MM-DD',
+            dirname: logDir + '/error',
+            filename: '%DATE%.error.log',
+            maxFiles: 30,
+            zippedArchive: true,
+        }),
+    ],
+});
+
+if (process.env.NODE_ENV === 'local') {
+    logger.add(new winston.transports.Console({
+        format: logFormat,
+        level: config.logLevel
+    }));
+}
+
+module.exports = {
+    logger
+};
